b966c7c38b5558803a632b45a018a0d9cdf83d58
Saul Calderon <saulcalderon1408@gmail.com>
2024-04-28 22:10:50 -0600

    Agregar conexion base de datos login

--- app/src/main/java/com/example/foro1/MainActivity.kt
+++ app/src/main/java/com/example/foro1/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.foro1
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foro1.db.HelperDB
+import com.example.foro1.model.Usuarios
 
 class MainActivity : AppCompatActivity() {
+
+    private var dbHelper: HelperDB? = null
+    private var db: SQLiteDatabase? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Conexion a base de datos
+        dbHelper = HelperDB(this)
+        db = dbHelper!!.writableDatabase
+
+        // Insertar usuarios por defecto
+        val instanciaUsuarios = Usuarios(this)
+        instanciaUsuarios.insertValuesDefault()
     }
 }
