6ea0c0ab59eaacdcf1a2f7868b83be162d1c4cae
Caleb-Lopez-G <calebverenicelopez@gmail.com>
2024-04-28 21:58:13 -0600

    Pantallas login

---
+++ app/src/main/java/com/example/foro1/db/RegUser.kt
@@ -0,0 +1,24 @@
+package com.example.foro1.db
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class RegUser: AppCompatActivity() {
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var registerButton: Button
+    private lateinit var userDatabaseHelper: HelperDB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/foro1/model/Productos.kt
+++ app/src/main/java/com/example/foro1/model/Productos.kt
@@ -10,8 +10,6 @@ class Productos(context: Context?) {
 
     private var helper: HelperDB? = null
     private var db: SQLiteDatabase? = null
-    data class Producto(val id: Int, val nombre: String, val precio: Double, val rutaImagen: String)
-
 
     init {
         helper = HelperDB(context)
@@ -26,41 +24,35 @@ class Productos(context: Context?) {
         val COL_ID = "idProducto"
         val COL_NOMBRE_PRODUCTO = "nombreProducto"
         val COL_PRECIO = "precio"
-        val COL_RUTA_IMAGEN = "rutaImagen"
 
         // sentencia SQL para crear la tabla
         val CREATE_TABLE_PRODUCTOS = (
                 "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_PRODUCTOS + "("
                         + COL_ID + " integer primary key autoincrement,"
                         + COL_NOMBRE_PRODUCTO + " varchar(100) NOT NULL,"
-                        + COL_PRECIO + " real NOT NULL,"
-                        + COL_RUTA_IMAGEN + " varchar(100))"
+                        + COL_PRECIO + " real NOT NULL)"
                 )
     }
 
     fun generarContentValues(
         nombreProducto: String?,
-        precio: Double,
-        rutaImagen: String?
+        precio: Double
     ): ContentValues {
         val valores = ContentValues()
         valores.put(COL_NOMBRE_PRODUCTO, nombreProducto)
         valores.put(COL_PRECIO, precio)
-        valores.put(COL_RUTA_IMAGEN, rutaImagen)
         return valores
     }
 
     fun insertValuesDefault() {
         val productos = arrayOf(
-            arrayOf("Manzana", 1.5, "manzana"),
-            arrayOf("Banana", 2.0, "banana"),
-            arrayOf("Pera", 1.0, "pera"),
-            arrayOf("Naranja", 1.0, "naranja"),
-            arrayOf("Requeson", 3.0, "requeson")
+            arrayOf("Manzana", 1.5),
+            arrayOf("Banana", 2.0),
+            arrayOf("Naranja", 1.0)
         )
 
         // Verificación si existen registros precargados
-        val columns = arrayOf(COL_ID, COL_NOMBRE_PRODUCTO, COL_PRECIO, COL_RUTA_IMAGEN)
+        val columns = arrayOf(COL_ID, COL_NOMBRE_PRODUCTO, COL_PRECIO)
         val cursor: Cursor? = db?.query(TABLE_NAME_PRODUCTOS, columns, null, null, null, null, null)
 
         // Validando que se ingrese la información solamente una vez, cuando se instala por primera vez la aplicación
@@ -69,15 +61,14 @@ class Productos(context: Context?) {
             for (producto in productos) {
                 val nombreProducto = producto[0] as String?  // Conversión explícita a String
                 val precio = producto[1] as Double         // Conversión explícita a Double
-                val rutaImagen = producto[2] as String?    // Conversión explícita a String
-                db?.insert(TABLE_NAME_PRODUCTOS, null, generarContentValues(nombreProducto, precio, rutaImagen))
+                db?.insert(TABLE_NAME_PRODUCTOS, null, generarContentValues(nombreProducto, precio))
             }
         }
         cursor?.close()
     }
 
     fun showAllProductos(): Cursor? {
-        val columns = arrayOf(COL_ID, COL_NOMBRE_PRODUCTO, COL_PRECIO, COL_RUTA_IMAGEN)
+        val columns = arrayOf(COL_ID, COL_NOMBRE_PRODUCTO, COL_PRECIO)
         return db?.query(
             TABLE_NAME_PRODUCTOS, columns,
             null, null, null, null, null
@@ -85,7 +76,7 @@ class Productos(context: Context?) {
     }
 
     fun searchProducto(nombreProducto: String): Cursor? {
-        val columns = arrayOf(COL_ID, COL_NOMBRE_PRODUCTO, COL_PRECIO, COL_RUTA_IMAGEN)
+        val columns = arrayOf(COL_ID, COL_NOMBRE_PRODUCTO, COL_PRECIO)
         return db?.query(
             TABLE_NAME_PRODUCTOS, columns,
             "$COL_NOMBRE_PRODUCTO=?", arrayOf(nombreProducto), null, null, null
